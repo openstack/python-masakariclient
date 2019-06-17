19c1d1a683f6543c2c8a83850223c970901fbe1c
akhiljain23 <akhil.jain@india.nec.com>
2019-06-17 15:53:45 +0530

    [Gate Fix]Switch to stestr
    According to Openstack summit session [1],
    stestr is maintained project to which all Openstack projects should migrate.
    Let's switch to stestr as other projects have already moved to it.
    
    This commit also updates Sphinx version in requirement.txt
    Also, switch from oslosphinx to openstackdocstheme
    
    [1] https://etherpad.openstack.org/p/YVR-python-pti
    
    Change-Id: I1d4071d5154f1f24f57b05a46771f9c243bb56b6

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,9 +21,9 @@ sys.path.insert(0, os.path.abspath('../..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    'openstackdocstheme',
+    'sphinx.ext.autodoc'
     #'sphinx.ext.intersphinx',
-    'oslosphinx'
 ]
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
@@ -55,7 +55,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
 # html_theme_path = ["."]
-# html_theme = '_theme'
+html_theme = 'openstackdocs'
 # html_static_path = ['static']
 
 # Output file base name for HTML help builder.
@@ -73,3 +73,10 @@ latex_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 #intersphinx_mapping = {'http://docs.python.org/': None}
+
+# -- Options for openstackdocstheme ---------------------------------------
+
+repository_name = 'openstack/python-masakariclient'
+bug_project = 'python-masakariclient'
+bug_tag = ''
+
--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -38,7 +38,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'oslosphinx',
+    'openstackdocstheme',
     'reno.sphinxext',
 ]
 
@@ -111,7 +111,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'openstackdocs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -191,6 +191,11 @@ html_static_path = ['_static']
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'MasakariClientReleaseNotesdoc'
 
+# -- Options for openstackdocstheme ---------------------------------------
+
+repository_name = 'openstack/python-masakariclient'
+bug_project = 'python-masakariclient'
+bug_tag = ''
 
 # -- Options for LaTeX output ---------------------------------------------
 
